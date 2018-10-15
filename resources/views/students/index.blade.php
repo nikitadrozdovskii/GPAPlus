ebf3a499f143e248dd75350afed77146b9e511b9
nikitadrozdovskii <butch386@gmail.com>
2018-10-15 08:00:46 -0500

    added color-coded averages for student grades

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,16 +6,24 @@
     @if(count($data['students'])>0)
         @foreach($data['students'] as $student)
             <div class="well">
-                First: {{$student->fname}}
-                Last: {{$student->lname}} 
-                Average assignment grade: {{$data['averages'][$student->id]}}
-                <div>
+                <span class="inline_heading">Name:</span> {{$student->fname}}{{$student->lname}} 
+                <span class="inline_heading">Average assignment grade:</span> 
+                @if($data['averages'][$student->id]<70)
+                <span class="F badge badge-secondary">{{$data['averages'][$student->id]}} - F</span>         
+                @elseif($data['averages'][$student->id]<80)
+                <span class="C badge badge-secondary">{{$data['averages'][$student->id]}} - C</span> 
+                @elseif($data['averages'][$student->id]<90)
+                <span class="B badge badge-secondary">{{$data['averages'][$student->id]}} - B</span> 
+                @elseif($data['averages'][$student->id]<101)
+                <span class="A badge badge-secondary align-bottom">{{$data['averages'][$student->id]}} - A</span>  
+                @endif       
+                {{-- <div> --}}
                         <a class="btn btn-primary" href="/students/{{$student->id}}/edit" role="button">Edit</a>
                         {!!Form::open(['action'=>['StudentsController@destroy', $student->id], 'method' => 'POST'])!!}
                             {{Form::hidden('_method', 'DELETE')}}
                             {{Form::submit('Remove', ['class' => 'btn btn-danger btn-outline'])}}
                         {!!Form::close()!!}
-                </div>
+                {{-- </div> --}}
 
                 <hr>
             </div>
